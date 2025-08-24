bfd2c42e380f39509d18d0266801f25046c76fc2
rene-0 <rene.colombaro@hotmail.com>
2025-08-23 21:55:38 -0300

    feat: change header to Authorization

--- src/main/adapters/express-route-adapters.ts
+++ src/main/adapters/express-route-adapters.ts
@@ -1,13 +1,13 @@
-import { Controller } from '../../presentation/protocols/controller'
 import { Request, Response } from 'express'
+import { Controller } from '../../presentation/protocols/controller'
 
 export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
-    const accessToken = req?.headers['x-access-token']
+    const accessToken = req?.headers['Authorization'] || req?.headers['authorization']
     const request = {
       ...(req.body || {}),
       ...(req.params || {}),
-      accessToken
+      accessToken,
     }
     const httpResponse = await controller.handle(request)
     res.status(httpResponse.statusCode).json(httpResponse.body)
