98bcd652729e4738c47beb2e6e99534e41f7f641
rene-0 <rene.colombaro@hotmail.com>
2025-08-26 22:16:34 -0300

    fix: add missing isAuth middleware

--- src/main/routes/product-routes.ts
+++ src/main/routes/product-routes.ts
@@ -2,7 +2,8 @@ import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapters'
 import { makeGetProductController } from '../factories/controllers/http/product/get-product-controller-factory'
 import { makeGetProductsController } from '../factories/controllers/http/product/get-products-controller-factory'
+import { isAuth } from '../middlewares/http/authentication/auth-middleware'
 export default (router: Router): void => {
-  router.get('/products/:id', adaptRoute(makeGetProductController()))
-  router.get('/products', adaptRoute(makeGetProductsController()))
+  router.get('/products/:id', isAuth, adaptRoute(makeGetProductController()))
+  router.get('/products', isAuth, adaptRoute(makeGetProductsController()))
 }
