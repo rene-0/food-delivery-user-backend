7f13287b26afa844ebbb8e842870e2a2207772a7
rene-0 <rene.colombaro@hotmail.com>
2025-08-26 22:32:53 -0300

    fix: update accessToken header to use 'authorization'

--- src/main/adapters/express-middleware-adapter.ts
+++ src/main/adapters/express-middleware-adapter.ts
@@ -4,7 +4,7 @@ import { Middleware } from '../../presentation/protocols/middleware'
 export const adaptMiddleware = (middleware: Middleware) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const request = {
-      accessToken: req.headers?.['x-access-token'],
+      accessToken: req.headers?.['authorization'],
       ...(req.headers || {}),
     }
     const httpResponse = await middleware.handle(request)
