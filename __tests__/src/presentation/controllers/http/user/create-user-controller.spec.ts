0aed4bd72e4f6503b9c7b53546c122dde82d4cbb
rene-0 <rene.colombaro@hotmail.com>
2025-09-16 11:43:19 -0300

    test: add create user controller test

---
+++ __tests__/src/presentation/controllers/http/user/create-user-controller.spec.ts
@@ -0,0 +1,41 @@
+import { CreateUserController } from '../../../../../../src/presentation/controllers/http/user/create-user-controller'
+import { CreateUserMock } from '../../../../../mocks/data/use-case/user/create-user-mock'
+
+describe('CreateUserController', () => {
+  it('should return badRequest if validation fails', async () => {
+    const sut = new CreateUserController(new CreateUserMock())
+    const response = await sut.handle({} as unknown as any) // missing id
+    expect(response.statusCode).toBe(400)
+  })
+
+  it('should return badRequest e-mail already exists', async () => {
+    const createUser = new CreateUserMock()
+    jest.spyOn(createUser, 'createUser').mockResolvedValueOnce(null)
+    const sut = new CreateUserController(createUser)
+    const response = await sut.handle({
+      name: 'any_name',
+      email: 'any_email@hotmail.com',
+      password: 'any_password',
+      phoneNumber: 'any_phoneNumber',
+    })
+    expect(response.statusCode).toBe(400)
+  })
+
+  it('should return ok if user is created', async () => {
+    const sut = new CreateUserController(new CreateUserMock())
+    const response = await sut.handle({
+      name: 'any_name',
+      email: 'any_email@hotmail.com',
+      password: 'any_password',
+      phoneNumber: 'any_phoneNumber',
+    })
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual({
+      name: 'any_name',
+      email: 'any_email@hotmail.com',
+      phoneNumber: 'any_phoneNumber',
+      createdAt: '1998-01-01',
+      updatedAt: '1998-01-01',
+    })
+  })
+})
