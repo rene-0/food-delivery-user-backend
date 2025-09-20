87ed37f89b5160ea4a43acb9e8cf8b97a149b3f0
rene-0 <rene.colombaro@hotmail.com>
2025-09-20 16:58:44 -0300

    fix: add auth to user update

--- src/main/routes/user-routes.ts
+++ src/main/routes/user-routes.ts
@@ -2,7 +2,8 @@ import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapters'
 import { makeCreateUserController } from '../factories/controllers/http/user/create-user-controller-factory'
 import { makeUpdateUserController } from '../factories/controllers/http/user/update-user-controller-factory'
+import { isAuth } from '../middlewares/http/authentication/auth-middleware'
 export default (router: Router): void => {
   router.post('/users', adaptRoute(makeCreateUserController()))
-  router.put('/users/:id', adaptRoute(makeUpdateUserController()))
+  router.put('/users', isAuth, adaptRoute(makeUpdateUserController()))
 }
