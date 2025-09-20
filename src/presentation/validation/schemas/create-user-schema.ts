daa3d317f0d0d15dcfa54022661324a09a036520
rene-0 <rene.colombaro@hotmail.com>
2025-09-20 16:59:03 -0300

    feat: add regex validation to phone number

--- src/presentation/validation/schemas/create-user-schema.ts
+++ src/presentation/validation/schemas/create-user-schema.ts
@@ -5,5 +5,5 @@ export const createUserSchema = Joi.object<CreateUserController.Request, true>({
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
-  phoneNumber: Joi.string().min(10).max(15).required(),
+  phoneNumber: Joi.string().regex(/^\d*$/).min(10).max(15).required(),
 })
