19d5e509873e76086793606baa554a37f76cffe5
rene-0 <rene.colombaro@hotmail.com>
2025-10-05 11:19:04 -0300

    feat: update content type middleware to set JSON response for API paths

--- src/main/middlewares/http/content-type.ts
+++ src/main/middlewares/http/content-type.ts
@@ -1,5 +1,7 @@
-import { Request, Response, NextFunction } from 'express'
+import { NextFunction, Request, Response } from 'express'
 export const contentType = (req: Request, res: Response, next: NextFunction): void => {
-  res.type('json')
+  if (req.path.startsWith('/api')) {
+    res.type('json')
+  }
   next()
 }
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,6 @@
+import express from 'express'
 import http from 'http'
+import path from 'path'
 import { SequelizeHelper } from '../infra/db/postgress/helpers/sequelize-helper'
 
 const port = process.env.SERVER_PORT
@@ -6,6 +8,7 @@ SequelizeHelper.connect()
   .then(async () => {
     const app = (await import('./config/app')).default
     const httpServer = http.createServer(app)
+    app.use('/public/uploads', express.static(path.join(__dirname, '..', '..', 'public', 'uploads')))
     httpServer.listen(port)
     console.log(`Server running at http://localhost:${port}`)
   })
